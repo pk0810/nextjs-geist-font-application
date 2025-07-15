b945b9fc913ffd3ce0b7873544beabdde3364fb7
pk0810 <40767127+pk0810@users.noreply.github.com>
2025-07-16 02:57:08 +0530

    Add WebAppMVC/Models/RegisterViewModel.cs 1752614828020

---
+++ WebAppMVC/Models/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppMVC.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(100, ErrorMessage = "Email must be less than 100 characters")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [StringLength(100, ErrorMessage = "First name must be less than 100 characters")]
+        public string? FirstName { get; set; }
+
+        [StringLength(100, ErrorMessage = "Last name must be less than 100 characters")]
+        public string? LastName { get; set; }
+    }
+}
